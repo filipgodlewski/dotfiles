881d6e12fc04ec04a28b866df80a537dee9ebed5
Filip Godlewski <filip.godlewski@outlook.com>
2020-05-22 20:06:52 +0200

    feat(zsh): add new git alias
    Adds `gdiffs` alias that fuzzy finds git diffs and shows them nicely.

--- .zsh/.functions.zsh
+++ .zsh/.functions.zsh
@@ -21,6 +21,11 @@ gamp() {
         gp
 }
 
+gdiffs() {
+        preview="git diff $@ --color=always -- {-1}"
+        git diff $@ --name-only | fzf -m --ansi --preview $preview
+}
+
 put() {
         touch $1
         v $1

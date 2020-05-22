25e6a67256df17a59bee51c9cd5309044b059910
Filip Godlewski <filip.godlewski@outlook.com>
2020-05-22 18:21:54 +0200

    feat(zsh): add ripgrep config file
    Adds `.ripgreprc` with first custom options

--- .zsh/.exports.zsh
+++ .zsh/.exports.zsh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-export LC_ALL=en_US.UTF-8
-export MANPAGER="/bin/sh -c \"col -b | vim --not-a-term -c 'set ft=man ts=8 nomod nolist noma' -\""
 export EDITOR="vim"
 export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob '!.git'"
+export LC_ALL=en_US.UTF-8
+export MANPAGER="/bin/sh -c \"col -b | vim --not-a-term -c 'set ft=man ts=8 nomod nolist noma' -\""
+export RIPGREP_CONFIG_PATH=~/.config/.ripgreprc

d7c616f51e7be5af5e52e0ddff82ca73084a3ced
Filip Godlewski <filip.godlewski@outlook.com>
2020-08-06 00:18:03 +0300

    adds remaining files

---
+++ .config/zsh/plugins.zsh
@@ -0,0 +1,4 @@
+source $ZPLUGINSDIR/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+source $ZPLUGINSDIR/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+source $ZPLUGINSDIR/zsh-completions/zsh-completions.plugin.zsh
+source $ZPLUGINSDIR/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh

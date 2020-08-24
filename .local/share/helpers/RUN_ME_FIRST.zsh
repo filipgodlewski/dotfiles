6c3fb8ad7b5fdc6a4761e614eb81888e9fe28573
Filip Godlewski <filip.godlewski@outlook.com>
2020-08-24 13:21:57 +0200

    add cfg local config prompts

--- .local/share/helpers/RUN_ME_FIRST.zsh
+++ .local/share/helpers/RUN_ME_FIRST.zsh
@@ -27,8 +27,16 @@ pip install -U pip setuptools wheel
 pip install -r $XDG_DATA_HOME/helpers/pip_list
 pyenv deactivate
 
-echo "\n>>> initialize git submodules <<<\n"
+echo "\n>>> initialize git submodules\n"
 cfg submodule update --init --recursive
+
+echo "\n>>> setup local git (cfg) config <<<\n"
+echo -n "Git local user name: "
+read cfg_username
+echo -n "Git local user email: "
+read cfg_email
+cfg config --local user.name "$cfg_username"
+cfg config --local user.email "$cfg_email"
 
 echo "\n>>> DONE. Reexecuting zsh <<<\n"
 exec zsh

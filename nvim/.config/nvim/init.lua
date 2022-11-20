0a9012cbabea953d91f7fc542f1ab429f33b1d8f
Filip Godlewski <54432731+filipgodlewski@users.noreply.github.com>
2022-11-21 00:15:30 +0100

    feat: 🎸 add lua ftplugin for searching paths

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,25 +6,26 @@ vim.g.mapleader = " "
 vim.g.python3_host_prog = os.getenv "XDG_DATA_HOME" .. "/venvs/nvim/bin/python3"
 
 vim.opt.autowriteall = true
-vim.opt.complete = vim.opt.complete + "kspell"
+vim.opt.complete:append "kspell"
 vim.opt.cmdheight = 0
 vim.opt.confirm = true
 vim.opt.copyindent = true
 vim.opt.diffopt = { "filler", "vertical" }
 vim.opt.expandtab = true
 vim.opt.fileformat = "unix"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.grepformat = "%f:%l:%c:%m"
 vim.opt.grepprg = "rg --vimgrep --no-heading --case-sensitive --follow --word-regexp"
 vim.opt.hidden = true
 vim.opt.ignorecase = true
 vim.opt.inccommand = "split"
 vim.opt.laststatus = 3
-vim.opt.matchpairs = vim.opt.matchpairs + "<:>"
+vim.opt.matchpairs:append "<:>"
 vim.opt.mouse = "ar"
-vim.opt.nrformats = vim.opt.nrformats + "alpha"
+vim.opt.nrformats:append "alpha"
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.path = vim.opt.path + "**"
+vim.opt.path:append "**"
 vim.opt.pumheight = 15
 vim.opt.scrolloff = 4
 vim.opt.shiftround = true
@@ -42,7 +43,7 @@ vim.opt.termguicolors = true
 vim.opt.undofile = true
 vim.opt.undolevels = 10000
 vim.opt.updatetime = 50
-vim.opt.wrap = false
+vim.opt.wrap = true
 vim.opt.sessionoptions = { "blank", "buffers", "curdir", "folds", "help", "tabpages", "winsize", "winpos", "terminal" }
 -- vim.opt.splitkeep = "screen"  -- uncomment in nvim 0.9
 
